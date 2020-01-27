54f3adef9ff8f84d4e15cc31062fc5398f11c25c
E Bauer <emily.bauer@bracari.com>
2020-01-27 12:05:13 -0500

    Adding support for parsing out and decoding PDF attachment.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import base64
 from validator import Validator
+from parser import Parser
 
 if len(sys.argv) != 2:
     print('Requires file path to XML file to validate')
@@ -14,3 +16,13 @@ else:
     print('Not valid! :(')
     #print(validator.getLastError())
     validator.printErrors()
+    sys.exit(1)
+
+parser = Parser(sys.argv[1])
+encodedAttachment = parser.getNodeValue("/cld:Claim/cld:Attachments/nc:Attachment/nc:BinaryBase64Object")
+print(encodedAttachment)
+decodedAttachment = bytearray(base64.b64decode(encodedAttachment))
+
+f = open("test.pdf", "w+b")
+f.write(decodedAttachment)
+f.close()
---
+++ parser.py
@@ -0,0 +1,34 @@
+from lxml import etree
+
+class Parser:
+
+    def __init__(self, xml_path: str):
+        # xmldoc is an ElementTree
+        self.xmldoc = etree.parse(xml_path)
+
+    def getNodeValue(self, xml_node: str) -> str:
+        namespace = 'http://vler.va.gov/vler/schemas/health/clinicalDocuments/clinicalAssessments/cpExams/Claim/2.0'
+        # root is an Element
+        root = self.xmldoc.getroot()
+
+        for child in root:
+            print(child.tag)
+
+        #result = root.findtext(xml_node)
+        #print(result[0].tag)
+
+        elm = self.xmldoc.xpath(xml_node, namespaces={'cld:':'http://vler.va.gov/vler/schemas/health/clinicalDocuments/clinicalAssessments/cpExams/Claim/2.0',
+            'cld':'http://vler.va.gov/vler/schemas/health/clinicalDocuments/clinicalAssessments/cpExams/Claim/2.0',
+        	'nc':'http://niem.gov/niem/niem-core/2.0',
+        	'vler':'http://va.gov/vler/schemas/vlerSupersetSchema/1.0/vler',
+        	'niem-xsd':'http://niem.gov/niem/proxy/xsd/2.0',
+        	's':'http://niem.gov/niem/structures/2.0',
+        	'xsi':'http://www.w3.org/2001/XMLSchema-instance',
+        	'pdshr':'http://vler.va.gov/vler/schemas/health/clinicalDocuments/clinicalAssessments/cpExams/PostDeploymentSeparationHealthReassessment/2.0'})
+        #print(elm[0].tag)
+        #print(elm[0].text)
+
+        return elm[0].text
+
+        # To print the whole document:
+        #print(etree.tostring(root, pretty_print=True))
